450216f9a04c8b9138badcf49c745b9c34bc2eb5
SayatP <sayat.petrosyan.sh@gmail.com>
2023-04-19 13:31:25 +0400

    mock order processor

--- project/back-end/orders/consumer.py
+++ project/back-end/orders/consumer.py
@@ -1,17 +1,19 @@
-from confluent_kafka import Consumer
-from json import loads
+import json
+from kafka import KafkaConsumer
 from time import sleep
 
-conf = {'bootstrap.servers': "kafka:9092",
-        'group.id': "orders-processor",
-        'auto.offset.reset': 'smallest'}
+from lib import order_processor
 
-c = Consumer(conf)
+consumer = KafkaConsumer(
+    'orders',
+    bootstrap_servers=['kafka:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='order-processor',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    api_version=(0, 10, 1)
+)
 
-c.subscribe(['orders'])
-
-while True:
-    print("###################")
-    msg = c.poll()
-    print(msg)
-    print(msg.value())
+for event in consumer:
+    order_processor.process(event.value)
+    print(event.value)
